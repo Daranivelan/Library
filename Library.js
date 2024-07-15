f2b5376009e715495580412cc43b4911c5cbb9de
Daranivelan <daranivelan10@gmail.com>
2024-07-15 20:52:54 +0530

    Initial Files

---
+++ Library.js
@@ -0,0 +1,441 @@
+let bookData = [
+    {
+        img: "Book1.jpeg",
+        name: "Pride and Prejudice",
+        Author: "Jane Austen",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Pride_and_Prejudice",
+        renew: "Renew"
+    },
+    {
+        img: "Book2.jpeg",
+        name: "To Kill a Mockingbird",
+        Author: "Harper Lee",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/To_Kill_a_Mockingbird",
+        renew: "Renew"
+    },
+    {
+        img: "Book3.jpeg",
+        name: "1984",
+        Author: "George Orwell",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Nineteen_Eighty-Four",
+        renew: "Renew"
+    },
+    {
+        img: "Book4.jpeg",
+        name: "The Great Gatsby",
+        Author: "F. Scott Fitzgerald",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/The_Great_Gatsby",
+        renew: "Renew"
+    },
+    {
+        img: "Book5.jpeg",
+        name: "One Hundred Years of Solitude",
+        Author: "Gabriel García Márquez",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/One_Hundred_Years_of_Solitude",
+        renew: "Renew"
+    },
+    {
+        img: "Book6.jpeg",
+        name: "Don Quixote",
+        Author: "Miguel de Cervantes",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Don_Quixote",
+        renew: "Renew"
+    },
+    {
+        img: "Book7.jpeg",
+        name: "The Lord of the Rings",
+        Author: "JRR Tolkien",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/The_Lord_of_the_Rings",
+        renew: "Renew"
+    },
+    {
+        img: "Book8.jpeg",
+        name: "Wuthering Heights",
+        Author: "Emily Brontë",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Wuthering_Heights",
+        renew: "Renew"
+    },
+    {
+        img: "Book9.jpeg",
+        name: "Jane Eyre",
+        Author: "Charlotte Brontë",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Jane_Eyre",
+        renew: "Renew"
+    },
+    {
+        img: "Book10.jpeg",
+        name: "Anna Karenina",
+        Author: "Leo Tolstoy",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Anna_Karenina",
+        renew: "Renew"
+    },
+    {
+        img: "Book11.jpeg",
+        name: "Frankenstein",
+        Author: "Mary Shelley",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Frankenstein",
+        renew: "Renew"
+    },
+    {
+        img: "Book12.jpeg",
+        name: "The Catcher in the Rye",
+        Author: "J. D. Salinger",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/The_Catcher_in_the_Rye",
+        renew: "Renew"
+    },
+    {
+        img: "Book13.jpeg",
+        name: "Animal Farm",
+        Author: "George Orwell",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Animal_Farm",
+        renew: "Renew"
+    },
+    {
+        img: "Book14.jpeg",
+        name: "Crime and Punishment",
+        Author: "Fyodor Dostoevsky",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Crime_and_Punishment",
+        renew: "Renew"
+    },
+    {
+        img: "Book15.jpeg",
+        name: "Fahrenheit 451",
+        Author: "Ray Bradbury",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Fahrenheit_451",
+        renew: "Renew"
+    },
+    {
+        img: "Book16.jpeg",
+        name: "Great Expectations",
+        Author: "Charles Dickens",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Great_Expectations",
+        renew: "Renew"
+    },
+    {
+        img: "Book17.jpeg",
+        name: "Lolita",
+        Author: "Vladimir Nabokov",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Lolita",
+        renew: "Renew"
+    },
+    {
+        img: "Book18.jpeg",
+        name: "Moby-Dick",
+        Author: "Herman Melville",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Moby-Dick",
+        renew: "Renew"
+    },
+    {
+        img: "Book19.jpeg",
+        name: "The Kite Runner",
+        Author: "Khaled Hosseini",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/The_Kite_Runner",
+        renew: "Renew"
+    },
+    {
+        img: "Book20.jpeg",
+        name: "Beloved",
+        Author: "Toni Morrison",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Beloved_(novel)",
+        renew: "Renew"
+    },
+    {
+        img: "Book21.jpeg",
+        name: "Little Women",
+        Author: "Louisa May Alcott",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/Little_Women",
+        renew: "Renew"
+    },
+    {
+        img: "Book22.jpeg",
+        name: "The Book Thief",
+        Author: "Markus Zusak",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/The_Book_Thief",
+        renew: "Renew"
+    },
+    {
+        img: "Book23.jpeg",
+        name: "The Little Prince",
+        Author: "Antoine de Saint-Exupéry",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/The_Little_Prince",
+        renew: "Renew"
+    },
+    {
+        img: "Book24.png",
+        name: "War and Peace",
+        Author: "Leo Tolstoy",
+        status: "Borrow",
+        Due: "14 Days Left",
+        fine: 0,
+        link: "https://en.wikipedia.org/wiki/War_and_Peace",
+        renew: "Renew"
+    },
+];
+
+const loadBooks = () => {
+	let htmlData = ``;
+	let searchText = document.getElementById("mySearch").value.toLowerCase();
+	bookData.forEach((data, index) => {
+		if (data.name.toLowerCase().includes(searchText) || data.Author.toLowerCase().includes(searchText)) {
+			htmlData += `<div key="${index}" class="book" id="book">
+				<a href="${data.link}" target="_blank">
+				<img src="Assets/${data.img}">
+				</a>
+				<p>Name: ${data.name}</p>
+				<p>Author: ${data.Author}</p>
+				<button class="btn1 ${data.status === 'Borrowed' ? 'borrowed-btn' : ''}" onclick="myButtonClicked(${index})">${data.status}</button>
+			</div>`;
+		}
+	});
+	document.getElementById("books").innerHTML = htmlData ? htmlData : "No books found";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  loadBooks();
+
+	const myBooksBtn = document.querySelector("#myBooksBtn");
+
+	myBooksBtn.addEventListener("click", () => {
+		loadMyBooks();
+		document.getElementById("bookSection").style.display = "block";
+	});
+
+    const loginBtn = document.querySelector("#loginBtn");
+
+    loginBtn.addEventListener("click", () => {
+		loadLoginPage();
+		document.getElementById("login").style.display = "block";
+	});
+
+    const signupBtn = document.querySelector("#signupBtn");
+
+    signupBtn.addEventListener("click", () => {
+		loadSignupPage();
+		document.getElementById("signup").style.display = "block";
+	});
+}, false);
+
+const myButtonClicked = (index) => {
+	bookData[index].status = 'Borrowed';
+	loadBooks();
+}
+
+const loadMyBooks = () => {
+	let htmlData = ``;
+	bookData.forEach((data, index) => {
+		if (data.status === 'Borrowed') {
+			htmlData += `<div key="${index}" class="book" id="book">
+				<a href="${data.link}" target="_blank">
+				<img src="Assets/${data.img}">
+				</a>
+				<p>Name: ${data.name}</p>
+				<p>Author: ${data.Author}</p>
+                <p>Due: ${data.Due}</p>
+                <p>Fine: Rs.${data.fine}</p>
+                <button class="btn1 ${data.renew === 'Renewed' ? 'borrowed-btn' : ''}" onclick="myRenewButtonClicked(${index})">${data.renew}</button>
+                <p>
+                    <button class="btn1" onclick="myReturnButtonClicked(${index})">Return</button>
+                </p>
+			</div>`;
+		}
+	});
+	document.getElementById("myBooks").innerHTML = htmlData ? htmlData : "No books borrowed";
+}
+
+const myRenewButtonClicked = (index) => {
+	bookData[index].renew = 'Renewed';
+    bookData[index].Due = '28 Days Left';
+    loadMyBooks();
+}
+
+const myReturnButtonClicked = (index) => {
+	bookData[index].status = 'Borrow';
+    bookData[index].renew = 'Renew';
+    bookData[index].Due = '14 Days Left';
+    loadMyBooks();
+    loadBooks();
+}
+
+const loadLoginPage = () => {
+    let htmldata = ``;
+    htmldata += `<div>
+        <div class="userName">
+            <span>Username: </span>
+            <input id="username1" type="text" placeholder="EmailID..." required>
+        </div>
+        <div class="userName">
+            <span>Password: </span>
+            <input id="pass" type="password" placeholder="Password..." required>
+        </div>
+        <div class="loginBtn">
+            <button onclick="loginCloseModal(event)">Login</button>
+        </div>
+    </div>`;
+
+    document.getElementById("loginPage").innerHTML = htmldata;
+}
+
+const loadSignupPage = () => {
+    let htmldata = ``;
+    htmldata += `<form>
+        <div class="userName">
+            <span>FirstName: </span>
+            <input id="username" type="text" placeholder="Firstname...">
+        </div>
+        <div class="userName">
+            <span>LastName: </span>
+            <input id="username" type="text" placeholder="Lastname...">
+        </div>
+        <div class="userName">
+            <span>Address: </span>
+            <input id="username" type="text" placeholder="Lastname...">
+        </div>
+        <div class="userName">
+            <span>Phone.No: </span>
+            <input id="username" type="text" placeholder="Lastname...">
+        </div>
+        <div class="userName">
+            <span>EmailID: </span>
+            <input id="username" type="text" placeholder="EmailID...">
+        </div>
+        <div class="userName">
+            <span>Password: </span>
+            <input id="username" type="password" placeholder="Password...">
+        </div>
+        <div class="loginBtn">
+            <button type="submit" onclick="signupCloseModal(event)">SignUp</button>
+        </div>
+    </form>`;
+
+    document.getElementById("signupPage").innerHTML = htmldata;
+}
+
+const closeModal = () => {
+	document.getElementById("bookSection").style.display = "none";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("signup").style.display = "none";
+}
+
+const loginCloseModal = (e) => {
+    e.preventDefault();
+    const signin = document.querySelector("#username1");
+    const pass = document.querySelector("#pass");
+
+    let flag;
+    if (signin.value === "abcd@gmail.com" && pass.value === "12345")
+    {
+        flag = true;
+    }
+    else
+    {
+        flag = false;
+    }
+    
+    if (flag)
+    {
+        document.getElementById("login").style.display = "none";
+    }
+    else
+    {
+        alert("Please Fill all the Deatils... Or Invalid Login... ");
+        document.getElementById("login").style.display = "block";
+    }
+}
+
+const signupCloseModal = (e) => {
+    e.preventDefault();
+    const signup = document.querySelectorAll("#username");
+
+    let flag;
+    for (const x of signup)
+    {
+        if (x.value.trim() === "")
+        {
+            flag = false;
+            break;
+        }
+        else
+        {
+            flag = true;
+        }
+    }
+    
+    if (flag)
+    {
+        document.getElementById("signup").style.display = "none";
+    }
+    else
+    {
+        alert("Please Fill all the Deatils....");
+        document.getElementById("signup").style.display = "block";
+    }
+}
